a727167b7a74f1dd57070ce4e7087e4ad73e1160
David Siddle <siddav83@gmail.com>
2022-02-08 23:13:08 +0000

    add js

--- app.js
+++ app.js
@@ -15,8 +15,28 @@ todoButton.addEventListener('click',addTodo);
 
 //Functions
 
+//prevent the page from refreshing
 function addTodo(event) {
   event.preventDefault();
 }
 
+//ToDo Div
+const todoDiv = document.createElement('div');
+todoDiv.classList.add('todo');
 
+//Create LI
+const newTodo = document.createElement('li');
+newTodo.innerText = `hey`
+todoDiv.appendChild(newTodo);
+
+//Check mark button
+const completeButton = document.createElement('button')
+completeButton.innerHTML = '<i class="das fa-check" ></i>'
+completeButton.classList.add('completed-button');
+todoDiv.appendChild(completeButton);
+
+//trash mark button
+const trashButton = document.createElement('button')
+trashButton.innerHTML = '<i class="das fa-trash" ></i>'
+trashButton.classList.add('completed-button');
+todoDiv.appendChild(trashButton);
